5b7efada09295d58a1d3eb4561765ac83efc81bb
Pedro Suzuki <pedrosuzuki7@gmail.com>
2020-09-12 16:51:56 -0300

    Feat(Infra): Docker

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 const express = require("express");
+const mysql = require("mysql");
 
 const signinRouter = require("./signin");
 
 const app = express();
 
 app.use("/signin/", signinRouter);
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "techneesafra",
+  database: "snack_database",
+  port: 3306,
+});
 
-app.listen(3000);
+connection.connect();
 
-console.log("App listening on port 3000");
+app.get("/products", function (req, res) {
+  connection.query("SELECT * FROM products", function (error, results) {
+    if (error) {
+      throw error;
+    }
+
+    res.send(results.map((item) => ({ name: item.name, price: item.price })));
+  });
+});
+app.listen(3001);
+
+console.log("App listening on port 3001");
